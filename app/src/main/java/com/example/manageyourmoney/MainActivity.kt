5c9f5b161482db215ade072cc358d4c866fd1a66
Tushar <bodalwartushar@gmail.com>
2024-11-29 21:35:08 +0530

    added splash screen

---
+++ app/src/main/java/com/example/manageyourmoney/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.manageyourmoney
+
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.manageyourmoney.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        navigateFragment()
+    }
+    private fun navigateFragment() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        // Set up ActionBar with NavController
+        setupActionBarWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
---
+++ app/src/main/java/com/example/manageyourmoney/view/SplashFragment.kt
@@ -0,0 +1,30 @@
+package com.example.manageyourmoney.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.manageyourmoney.R
+import com.example.manageyourmoney.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+    private lateinit var binding:FragmentSplashBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding=FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnOnBoardingLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_splashFragment_to_signInFragment)
+        }
+
+    }
+}
